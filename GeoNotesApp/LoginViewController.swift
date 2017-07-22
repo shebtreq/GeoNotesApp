0373e323047c0654a828e6562cdc106b3a836b7a
S Trq <strq@Ss-MacBook-Air.local>
2017-07-23 09:46:52 +1000

    first commit

---
+++ GeoNotesApp/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GeoNotesApp
+//
+//  Created by S Trq on 23/7/17.
+//  Copyright © 2017 S Trq. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
